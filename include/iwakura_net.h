34803d82f3c8219ec21fbe2295dfaaca2bb64d47
willman <wrds.catto@gmail.com>
2026-04-29 19:14:26 -0400

    we got functionallity on all modules so time to trim code

--- include/iwakura_net.h
+++ include/iwakura_net.h
@@ -3,6 +3,10 @@
 
 #include <stdint.h>
 #include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdio.h>
 
 #define MAX_PAYLOAD 1024
 
@@ -35,5 +39,49 @@ typedef struct {
     uint32_t payload_len;
     char payload[MAX_PAYLOAD];
 } iwakura_msg_t;
+
+static inline void net_push_to_hub(iwakura_req_t target, const char* payload) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) return;
+
+    struct sockaddr_in hub_addr = { .sin_family = AF_INET, .sin_port = htons(get_iwakura_port()) };
+    inet_pton(AF_INET, get_iwakura_host(), &hub_addr.sin_addr);
+
+    if (connect(sock, (struct sockaddr *)&hub_addr, sizeof(hub_addr)) == 0) {
+        iwakura_msg_t msg;
+        memset(&msg, 0, sizeof(msg));
+        msg.type = UPDATE_DATA;
+        msg.target_type = target;
+        snprintf(msg.payload, MAX_PAYLOAD, "%s", payload);
+        msg.payload_len = strlen(msg.payload);
+        send(sock, &msg, sizeof(msg), 0);
+    }
+    close(sock);
+}
+
+static inline void net_fetch_from_hub(iwakura_req_t target, char* buffer, const char* fallback) {
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    if (sock < 0) {
+        strcpy(buffer, fallback);
+        return;
+    }
+
+    struct sockaddr_in hub_addr = { .sin_family = AF_INET, .sin_port = htons(get_iwakura_port()) };
+    inet_pton(AF_INET, get_iwakura_host(), &hub_addr.sin_addr);
+
+    strcpy(buffer, fallback);
+
+    if (connect(sock, (struct sockaddr *)&hub_addr, sizeof(hub_addr)) == 0) {
+        iwakura_msg_t msg;
+        memset(&msg, 0, sizeof(msg));
+        msg.type = target;
+        send(sock, &msg, sizeof(msg), 0);
+
+        if (recv(sock, &msg, sizeof(msg), 0) > 0 && strlen(msg.payload) > 0) {
+            snprintf(buffer, MAX_PAYLOAD, "%s", msg.payload);
+        }
+    }
+    close(sock);
+}
 
 #endif
