920e3ffab738e2e7038f4bf72027273df7051537
willman <wrds.catto@gmail.com>
2026-04-29 12:59:51 -0400

    also deleted fallbacks here, but it's not a bad idea to keep them either. todo: decide this for prod

--- include/iwakura_net.h
+++ include/iwakura_net.h
@@ -7,13 +7,15 @@
 #define MAX_PAYLOAD 1024
 
 static inline int get_iwakura_port() {
-    const char *p = getenv("IWAKURA_PORT");
-    return p ? atoi(p) : 9000;
+    return atoi(getenv("IWAKURA_PORT"));
 }
 
 static inline const char* get_iwakura_host() {
-    const char *h = getenv("IWAKURA_HOST");
-    return h ? h : "127.0.0.1";
+    return getenv("IWAKURA_HOST");
+}
+
+static inline int get_guestbook_port() {
+    return atoi(getenv("GUESTBOOK_PORT"));
 }
 
 typedef enum {
