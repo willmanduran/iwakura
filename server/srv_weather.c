fad05f14cbb55c2b045416affce5a6f1725e3528
willman <wrds.catto@gmail.com>
2026-04-30 17:22:55 -0400

    using a simple push to hub logic

--- server/srv_weather.c
+++ server/srv_weather.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include <string.h>
 #include <unistd.h>
-#include <arpa/inet.h>
 #include <curl/curl.h>
 #include <json-c/json.h>
 #include "../include/iwakura_net.h"
@@ -55,23 +54,11 @@ void fetch_and_push_weather() {
                 if(json_object_object_get_ex(cur, "weather_code", &o)) c = json_object_get_int(o);
                 if(json_object_object_get_ex(cur, "is_day", &o)) d = json_object_get_int(o);
 
-                int sock = socket(AF_INET, SOCK_STREAM, 0);
-                struct sockaddr_in hub_addr = { .sin_family = AF_INET, .sin_port = htons(get_iwakura_port()) };
-                inet_pton(AF_INET, get_iwakura_host(), &hub_addr.sin_addr);
+                char payload[MAX_PAYLOAD];
+                snprintf(payload, MAX_PAYLOAD, "%.1f|%.1f|%.1f|%d|%d", t, f, w, c, d);
 
-                if (connect(sock, (struct sockaddr *)&hub_addr, sizeof(hub_addr)) == 0) {
-                    iwakura_msg_t msg;
-                    memset(&msg, 0, sizeof(msg));
-                    msg.type = UPDATE_DATA;
-                    msg.target_type = REQ_WEATHER;
-
-                    snprintf(msg.payload, MAX_PAYLOAD, "%.1f|%.1f|%.1f|%d|%d", t, f, w, c, d);
-                    msg.payload_len = strlen(msg.payload);
-
-                    send(sock, &msg, sizeof(msg), 0);
-                    printf("[WEATHER] Pushed to Hub: %s\n", msg.payload);
-                }
-                close(sock);
+                net_push_to_hub(REQ_WEATHER, payload);
+                printf("[WEATHER] Pushed to Hub: %s\n", payload);
             }
             json_object_put(j);
         }
