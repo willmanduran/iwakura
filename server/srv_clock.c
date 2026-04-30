1f6b0fdc142f29ff798f15104486d87f399da226
willman <wrds.catto@gmail.com>
2026-04-30 17:06:04 -0400

    implemented dry now that logic works, but logic stays the same

--- server/srv_clock.c
+++ server/srv_clock.c
@@ -1,35 +1,20 @@
 #include <stdio.h>
-#include <string.h>
-#include <unistd.h>
-#include <arpa/inet.h>
 #include <time.h>
+#include <unistd.h>
 #include "../include/iwakura_net.h"
 
 int main() {
-    int port = get_iwakura_port();
-    const char *host = get_iwakura_host();
-    struct sockaddr_in serv_addr = { .sin_family = AF_INET, .sin_port = htons(port) };
-    inet_pton(AF_INET, host, &serv_addr.sin_addr);
-
-    printf("[CLOCK] Provider starting... targeting %s:%d\n", host, port);
+    printf("[CLOCK] Provider starting...\n");
 
     while (1) {
-        int sock = socket(AF_INET, SOCK_STREAM, 0);
-        if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) >= 0) {
-            time_t now = time(NULL);
-            struct tm *t = localtime(&now);
+        time_t now = time(NULL);
+        struct tm *t = localtime(&now);
 
-            iwakura_msg_t msg;
-            memset(&msg, 0, sizeof(msg));
-            msg.type = UPDATE_DATA;
-            msg.target_type = REQ_CLOCK;
+        char payload[MAX_PAYLOAD];
+        strftime(payload, MAX_PAYLOAD, "%H:%M|%A, %d %B", t);
 
-            strftime(msg.payload, MAX_PAYLOAD, "%H:%M|%A, %d %B", t);
-            msg.payload_len = strlen(msg.payload);
+        net_push_to_hub(REQ_CLOCK, payload);
 
-            send(sock, &msg, sizeof(msg), 0);
-            close(sock);
-        }
         sleep(1);
     }
     return 0;
