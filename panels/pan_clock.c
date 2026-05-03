b3cb59c160e169b1337ec6b45d2b18172b2b4084
willman <wrds.catto@gmail.com>
2026-05-03 01:21:36 -0400

    this is where we decide to go with just pipe-delimited data type, this time as time and date data.

--- panels/pan_clock.c
+++ panels/pan_clock.c
@@ -5,23 +5,15 @@
 
 int main() {
     char raw_data[MAX_PAYLOAD];
-    char display_frame[MAX_PAYLOAD];
+    char display_frame[MAX_PAYLOAD + 16];
 
     while (1) {
         net_fetch_from_hub(REQ_CLOCK, raw_data, "--:--|Syncing...");
 
-        char *time_str = strtok(raw_data, "|");
-        char *date_str = strtok(NULL, "|");
-
-        if (time_str && date_str) {
-            snprintf(display_frame, MAX_PAYLOAD, "CLOCK|\033[1;32m%s\033[0m \033[1;30m|\033[0m %s                                        ", time_str, date_str);
-        } else {
-            snprintf(display_frame, MAX_PAYLOAD, "CLOCK|Syncing...                                        ");
-        }
-
+        snprintf(display_frame, sizeof(display_frame), "CLOCK|%s", raw_data);
         net_push_to_orchestrator(display_frame);
+
         sleep(1);
     }
-
     return 0;
 }
